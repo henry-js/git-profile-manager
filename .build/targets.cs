9926dda107a92ac9e93b97fe55539d0b80971688
henry-js <79054685+henry-js@users.noreply.github.com>
2025-09-11 13:15:34 +0100

    build: add new build template

---
+++ .build/targets.cs
@@ -0,0 +1,155 @@
+#!/usr/bin/dotnet run
+
+#:package McMaster.Extensions.CommandLineUtils@4.1.1
+#:package Bullseye@6.0.0
+#:package SimpleExec@12.0.0
+
+using Bullseye;
+using McMaster.Extensions.CommandLineUtils;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+using var app = new CommandLineApplication { UsePagerForHelpText = false };
+app.HelpOption();
+
+var solutionOption = app.Option<string>(
+    "-s|--solution <solution>",
+    "The solution file to operate on.",
+    CommandOptionType.SingleValue,
+    opts => opts.DefaultValue = "GitProfileManager.slnx"
+);
+var publishProjectOption = app.Option<string>(
+    "--publishProject <project>",
+    "The project file to publish as an application.",
+    CommandOptionType.SingleValue,
+    opts => opts.DefaultValue = "src/GitProfileManager/GitProfileManager.csproj"
+);
+var configurationOption = app.Option<string>(
+    "-c|--configuration <configuration>",
+    "The build configuration.",
+    CommandOptionType.SingleValue,
+    opts => opts.DefaultValue = "Release"
+);
+var ridOption = app.Option<string>(
+    "--rid <rid>",
+    "The runtime identifier (RID) to use for publishing.",
+    CommandOptionType.SingleValue
+);
+var versionOption = app.Option<string>(
+    "--version <version>",
+    "The version to use for packing.",
+    CommandOptionType.SingleValue
+);
+app.Argument(
+    "targets",
+    "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.",
+    true
+);
+foreach (var (aliases, description) in Options.Definitions)
+{
+    _ = app.Option(string.Join("|", aliases), description, CommandOptionType.NoValue);
+}
+
+app.OnExecuteAsync(async _ =>
+{
+    var root = Directory.GetCurrentDirectory();
+    var configuration = configurationOption.Value();
+    var solution = solutionOption.Value();
+
+    var targets = app.Arguments[0].Values.OfType<string>();
+    var options = new Options(
+        Options.Definitions.Select(d =>
+            (
+                d.Aliases[0],
+                app.Options.Single(o => d.Aliases.Contains($"--{o.LongName}")).HasValue()
+            )
+        )
+    );
+
+    Target(
+        "clean",
+        () =>
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(solution);
+            ArgumentException.ThrowIfNullOrWhiteSpace(configuration);
+            return RunAsync("dotnet", $"clean {solution} --configuration {configuration}");
+        }
+    );
+
+    Target(
+        "restore",
+        () =>
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(solution);
+
+            var rid = ridOption.Value();
+            var runtimeArg = string.IsNullOrEmpty(rid) ? $"--runtime {rid}" : string.Empty;
+
+            return RunAsync("dotnet", $"restore {solution} {runtimeArg}");
+        }
+    );
+
+    Target(
+        "build",
+        ["restore"],
+        () =>
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(solution);
+            ArgumentException.ThrowIfNullOrWhiteSpace(configuration);
+            return RunAsync(
+                "dotnet",
+                $"build {solution} --configuration {configuration} --no-restore"
+            );
+        }
+    );
+
+    Target(
+        "test",
+        ["build"],
+        async () =>
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(solution);
+            ArgumentException.ThrowIfNullOrWhiteSpace(configuration);
+
+            // Note: Code coverage requires .NET 10 SDK or later, and
+            // the extension package Microsoft.Testing.Extensions.CodeCoverage
+            var testResultFolder = "TestResults";
+            var coverageFileName = "coverage.xml";
+            var testResultPath = Directory.CreateDirectory(Path.Combine(root, testResultFolder));
+            await RunAsync(
+                "dotnet",
+                $"test --solution {solution} --configuration {configuration} --no-build --coverage --coverage-output {Path.Combine(testResultPath.FullName, coverageFileName)} --coverage-output-format xml --ignore-exit-code 8"
+            );
+        }
+    );
+
+    Target(
+        "default",
+        ["build"],
+        () => Console.WriteLine("Default target ran, which depends on 'build'.")
+    );
+
+    Target(
+        "publish",
+        dependsOn: ["build"],
+        () =>
+        {
+            var publishProject = publishProjectOption.Value();
+            ArgumentException.ThrowIfNullOrWhiteSpace(publishProject);
+
+            var rid = ridOption.Value();
+            var runtimeArg = string.IsNullOrEmpty(rid) ? $"--runtime {rid}" : string.Empty;
+
+            var publishDir = Path.Combine(root, "dist", "publish", rid!);
+
+            return RunAsync(
+                "dotnet",
+                $"publish {publishProject} -c {configuration} -o {publishDir} --no-build {runtimeArg}"
+            );
+        }
+    );
+
+    await RunTargetsAndExitAsync(targets, options);
+});
+
+return await app.ExecuteAsync(args);
